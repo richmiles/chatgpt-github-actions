c79e549cd75cda87b9ee9b6a5840c993f09be9b5
Rich Miles <me@richmiles.xyz>
2023-12-08 19:39:33 -0700

    Added some additional functions and types.

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # Container image that runs your code
-FROM python:3.8-slim
+FROM python:3.12-slim
 
 # Instalando as dependencias
 COPY requirements.txt .
--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 ## Import statements
 import argparse
+import string
 import openai
 import os
 import requests
@@ -9,13 +10,27 @@ from github import Github
 
 ## Adding command-line arguments
 parser = argparse.ArgumentParser()
-parser.add_argument('--openai_api_key', help='Your OpenAI API Key')
-parser.add_argument('--github_token', help='Your Github Token')
-parser.add_argument('--github_pr_id', help='Your Github PR ID')
-parser.add_argument('--openai_engine', default="text-davinci-002", help='GPT-3 model to use. Options: text-davinci-002, text-babbage-001, text-curie-001, text-ada-001')
-parser.add_argument('--openai_temperature', default=0.5, help='Sampling temperature to use. Higher values means the model will take more risks. Recommended: 0.5')
-parser.add_argument('--openai_max_tokens', default=2048, help='The maximum number of tokens to generate in the completion.')
-parser.add_argument('--mode', default="files", help='PR interpretation form. Options: files, patch')
+parser.add_argument("--openai_api_key", help="Your OpenAI API Key")
+parser.add_argument("--github_token", help="Your Github Token")
+parser.add_argument("--github_pr_id", help="Your Github PR ID")
+parser.add_argument(
+    "--openai_engine",
+    default="text-davinci-002",
+    help="GPT-3 model to use. Options: text-davinci-002, text-babbage-001, text-curie-001, text-ada-001",
+)
+parser.add_argument(
+    "--openai_temperature",
+    default=0.5,
+    help="Sampling temperature to use. Higher values means the model will take more risks. Recommended: 0.5",
+)
+parser.add_argument(
+    "--openai_max_tokens",
+    default=2048,
+    help="The maximum number of tokens to generate in the completion.",
+)
+parser.add_argument(
+    "--mode", default="files", help="PR interpretation form. Options: files, patch"
+)
 args = parser.parse_args()
 
 ## Authenticating with the OpenAI API
@@ -26,7 +41,7 @@ g = Github(args.github_token)
 
 
 def files():
-    repo = g.get_repo(os.getenv('GITHUB_REPOSITORY'))
+    repo = g.get_repo(os.getenv("GITHUB_REPOSITORY"))
     pull_request = repo.get_pull(int(args.github_pr_id))
 
     ## Loop through the commits in the pull request
@@ -35,42 +50,28 @@ def files():
         # Getting the modified files in the commit
         files = commit.files
         for file in files:
-                # Getting the file name and content
+            # Getting the file name and content
             filename = file.filename
-            try:                
-                content = repo.get_contents(filename, ref=commit.sha).decoded_content.decode('utf-8')
+            try:
+                content = repo.get_contents(
+                    filename, ref=commit.sha
+                ).decoded_content.decode("utf-8")
 
-                # Updated code to use ChatGPT completions
-                response = openai.chat.completions.create(
-                    model=args.openai_engine,
-                    messages=[
-                        {
-                            "role": "system",
-                            "content": "Please review the following code for clarity, efficiency, and adherence to best practices. Highlight any areas for improvement, suggest optimizations, and note potential bugs or security vulnerabilities. Also, consider the maintainability and scalability of the code."
-                        },
-                        {
-                            "role": "user",
-                            "content": content
-                        }
-                    ],
-                    temperature=float(args.openai_temperature),
-                    max_tokens=int(args.openai_max_tokens)
-                )
-                
-                # Accessing the completion text from the response
-                completion_text = response['choices'][0]['message']['content'] if response['choices'] else ''
+                review = get_code_review_from_openai(content)
 
                 # Adding a comment to the pull request with ChatGPT's response
                 pull_request.create_issue_comment(
-                    f"ChatGPT's response about `{file.filename}`:\n {completion_text}")
+                    f"ChatGPT's response about `{file.filename}`:\n {review}"
+                )
             except Exception as e:
                 error_message = str(e)
                 print(error_message)
-                pull_request.create_issue_comment(f"ChatGPT was unable to process the response about {filename}")
+                pull_request.create_issue_comment(error_message)
+                raise e
 
 
 def patch():
-    repo = g.get_repo(os.getenv('GITHUB_REPOSITORY'))
+    repo = g.get_repo(os.getenv("GITHUB_REPOSITORY"))
     pull_request = repo.get_pull(int(args.github_pr_id))
 
     content = get_content_patch()
@@ -89,41 +90,25 @@ def patch():
             file_name = diff_text.split("b/")[1].splitlines()[0]
             print(file_name)
 
-            # Updated code to use ChatGPT completions
-            response = openai.chat.completions.create(
-                model=args.openai_engine,
-                messages=[
-                    {
-                        "role": "system",
-                        "content": "Please review the following code for clarity, efficiency, and adherence to best practices. Highlight any areas for improvement, suggest optimizations, and note potential bugs or security vulnerabilities. Also, consider the maintainability and scalability of the code."
-                    },
-                    {
-                        "role": "user",
-                        "content": content
-                    }
-                ],
-                temperature=float(args.openai_temperature),
-                max_tokens=int(args.openai_max_tokens)
+            review = get_code_review_from_openai(diff_text)
+            # Adding a comment to the pull request with ChatGPT's response
+            pull_request.create_issue_comment(
+                f"ChatGPT's response about `{file_name}`:\n {review}"
             )
-            
-            # Accessing the completion text from the response
-            completion_text = response['choices'][0]['message']['content'] if response['choices'] else ''
-            
-            print(response)
-            print(completion_text)
         except Exception as e:
             error_message = str(e)
             print(error_message)
-            pull_request.create_issue_comment(f"ChatGPT was unable to process the response about {file_name}")
+            pull_request.create_issue_comment(error_message)
+            raise e
 
 
-def get_content_patch():
+def get_content_patch() -> str:
     url = f"https://api.github.com/repos/{os.getenv('GITHUB_REPOSITORY')}/pulls/{args.github_pr_id}"
     print(url)
 
     headers = {
-        'Authorization': f"token {args.github_token}",
-        'Accept': 'application/vnd.github.v3.diff'
+        "Authorization": f"token {args.github_token}",
+        "Accept": "application/vnd.github.v3.diff",
     }
 
     response = requests.request("GET", url, headers=headers)
@@ -134,8 +119,37 @@ def get_content_patch():
     return response.text
 
 
-if (args.mode == "files"):
+def get_code_review_from_openai(content: str) -> str:
+    try:
+        messages = [
+            {
+                "role": "system",
+                "content": "Please review the following code for clarity, efficiency, and adherence to best practices. Highlight any areas for improvement, suggest optimizations, and note potential bugs or security vulnerabilities. Also, consider the maintainability and scalability of the code.",
+            },
+            {"role": "user", "content": content},
+        ]
+        # Updated code to use ChatGPT completions
+        response = openai.chat.completions.create(
+            model=args.openai_engine,
+            messages=messages,
+            temperature=float(args.openai_temperature),
+            max_tokens=int(args.openai_max_tokens),
+        )
+
+        # Accessing the completion text from the response
+        completion_text = (
+            response["choices"][0]["message"]["content"] if response["choices"] else ""
+        )
+        return completion_text
+    except Exception as e:
+        error_message = str(e)
+        raise Exception(
+            f"ChatGPT was unable to process the response about {content}\n\n{error_message}\n\n{messages}\n\n{response}"
+        )
+
+
+if args.mode == "files":
     files()
 
-if (args.mode == "patch"):
+if args.mode == "patch":
     patch()
